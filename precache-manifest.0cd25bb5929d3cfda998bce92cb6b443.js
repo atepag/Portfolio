66904b9a8d0f55f7010e4c0c487083e72e676f70
Arturo Tepale <atepaleag@gmail.com>
2020-08-03 18:35:30 -0500

    Updates

---
+++ precache-manifest.0cd25bb5929d3cfda998bce92cb6b443.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ef9e7b962967afd6007d237a5ce677c",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "37aedd5ac011bd89b292",
+    "url": "/Portfolio/static/css/main.0683713c.chunk.css"
+  },
+  {
+    "revision": "6899c2cfa1e900b2c72d",
+    "url": "/Portfolio/static/js/2.d419c7ff.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Portfolio/static/js/2.d419c7ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37aedd5ac011bd89b292",
+    "url": "/Portfolio/static/js/main.ab9f2b5e.chunk.js"
+  },
+  {
+    "revision": "d4e27a8801375d3e1832",
+    "url": "/Portfolio/static/js/runtime-main.862de8c6.js"
+  },
+  {
+    "revision": "41d748c1ab9c858a1f990a94559814c5",
+    "url": "/Portfolio/static/media/Nebula-Regular.41d748c1.otf"
+  },
+  {
+    "revision": "5849473997d0e00c3ce56c08172c1c4f",
+    "url": "/Portfolio/static/media/as.58494739.png"
+  },
+  {
+    "revision": "b9d912352a4b5cdf057da195773e11cb",
+    "url": "/Portfolio/static/media/background.b9d91235.jpg"
+  },
+  {
+    "revision": "d41e50cd4ce59eac53a0bd1df517e15f",
+    "url": "/Portfolio/static/media/contracto.d41e50cd.png"
+  },
+  {
+    "revision": "042366f41259912f86c5745b97b608de",
+    "url": "/Portfolio/static/media/coronavirus.042366f4.svg"
+  },
+  {
+    "revision": "38578d2e01dbaf234ae2a5303c40d43b",
+    "url": "/Portfolio/static/media/fblogo.38578d2e.png"
+  },
+  {
+    "revision": "0c768863b084475e3760cf618d154919",
+    "url": "/Portfolio/static/media/git.0c768863.png"
+  },
+  {
+    "revision": "aada4327e965eee8cc3c624358214a31",
+    "url": "/Portfolio/static/media/insta.aada4327.png"
+  },
+  {
+    "revision": "228998e0a44e398dc4b2b419a2ed6b0b",
+    "url": "/Portfolio/static/media/linlogo.228998e0.png"
+  },
+  {
+    "revision": "28cda8fcc1710f166e0eff929a6b7d58",
+    "url": "/Portfolio/static/media/lookout.28cda8fc.png"
+  },
+  {
+    "revision": "0ba7ba62010f3211d77cc20246c8244a",
+    "url": "/Portfolio/static/media/me.0ba7ba62.jpg"
+  },
+  {
+    "revision": "fe8aa448f71bc9ce2997a444f890be71",
+    "url": "/Portfolio/static/media/puzzle.fe8aa448.png"
+  },
+  {
+    "revision": "189ecdf0f686fac97e761ce34b1332e6",
+    "url": "/Portfolio/static/media/uni.189ecdf0.png"
+  }
+]);
