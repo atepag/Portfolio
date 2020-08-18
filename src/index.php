a4a073c93e53dc7ebf5a0d7185f340f4b80c888f
Arturo Tepale <atepaleag@gmail.com>
2020-08-18 12:05:27 -0500

    First Version

--- src/index.php
+++ src/index.php
@@ -2,7 +2,7 @@
 
 header("Access-Control-Allow-Origin: https://atepag.github.io/Portfolio/");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, application/xml, X-PINGOTHER");
+header("Access-Control-Allow-Headers: Authorization, Content-Type, application/xml, X-PINGOTHER");
 header("Access-Control-Request-Method: POST");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Max-Age: 86400");
